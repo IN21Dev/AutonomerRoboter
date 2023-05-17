4a52a547a79884b313ac7fc1e035abbf51128cee
Jan Focke <janfocke12@outlook.de>
2023-05-17 11:26:27 +0200

    Hinzufügen von Projektdateien und Erste Funktion erstellen

---
+++ AutonomerRoboter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutonomerRoboter
+{
+    public class Comms
+    {
+        public static void BefehlGeben(string Ziel, string Arg1, string Arg2)
+        {
+            System.Diagnostics.Process.Start("https://" + Ziel + "/<name.py>?" + Arg1 + "=" + Arg2);
+
+            var BrowserProcess = Process.GetProcesses().
+            Where(pr => pr.ProcessName == "msedge"); // without '.exe'
+
+            foreach (var process in BrowserProcess)
+            {
+                process.Kill();
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comms.BefehlGeben("", "", "");
+        }
+    }
+}
