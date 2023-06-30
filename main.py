1303db1e68a10e1acd29dbe4cabc6eafc2e559cb
Endergamer1 <janfocke12@outlook.de>
2023-06-30 08:20:14 +0200

    entfernen von Kommentar

--- main.py
+++ main.py
@@ -7,31 +7,6 @@ CommandList = []
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=192.168.88.228\Storagebase;DATABASE=Storage Force;uid=sa;pwd=BSDGG.in21')
 cursor = cnxn.cursor()
 
-#file = open("C:\Service\Test.txt")
-#with open("C:\Service\Test.txt") as f:
-#    mylist = f.read().splitlines() 
-#    result = [item.split(",")for item in mylist]
-#cursor.execute("EXEC dbo.usp_Breadth_First 4,27")
-#test = cursor.fetchone()
-#test2 = [row for row in test]
-#test3 = test2[3]
-#test4 = test3.split(",")
-#Param1 = str(test4[1])
-#cursor.execute("EXEC dbo.usp_Breadth_First " + Param1 + "," + Ziel)
-#print("EXEC dbo.usp_Breadth_First " + Command1 + "," + Command2)
-#please = cursor.fetchone()
-#print(please)
-#Help = [x.strip(",") for x in CommandList[3]]
-#filter(None,Help)
-
-#for x in result:
-#    command1 ="'" + x[0] + "'"
-#    command2 ="'" + x[1] + "'"
-#    command3 ="'" + x[2] + "'"
-#    command4 ="'" + x[3] + "'"
-#    cursor.execute("INSERT INTO [dbo].[RoboPath] VALUES (" + command1 + "," + command2  + "," + command3  + "," + command4 + ")")
-#cnxn.commit()
-
 def WegFindung(RoboID):
     ID = str(RoboID)
     cursor.execute('SELECT StartPoint,ZielPoint FROM dbo.Roboter WHERE ID =' + ID)
