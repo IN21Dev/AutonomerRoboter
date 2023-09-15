968178467b39ca72051424d00d83aa7bc68effe9
Endergamer1 <janfocke12@outlook.de>
2023-09-15 10:19:20 +0200

    Hinzufügen von Tabellen Update Funktion

--- test.py
+++ test.py
@@ -6,4 +6,19 @@ import http.server
 import socketserver
 
 
-resp = urllib3.request("GET", "192.168.88.232:8000/?argument=7")
+CommandList = []
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=192.168.88.223\Storagebase;DATABASE=Storage Force;uid=sa;pwd=BSDGG.in21')
+cursor = cnxn.cursor()
+
+ID = str(1)
+cursor.execute('SELECT StartPoint,ZielPoint FROM dbo.Roboter WHERE ID =' + ID)
+testnow = cursor.fetchone()
+WegInfo = [row for row in testnow]
+
+
+cursor.execute("EXEC dbo.usp_Breadth_First " + WegInfo[0] + "," + WegInfo[1])
+QueryResult = cursor.fetchone()
+ListResult = [row for row in QueryResult]
+ZielResult = ListResult[3]        
+ZielPfad = ZielResult.split(",")
+print(ZielPfad)
